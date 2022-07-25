f8805e92671be5eeb16275cbc588cadd7f5516d8
Lukian <lukianbogdanets@ufl.edu>
2022-07-24 22:22:09 -0400

    Moved Components folder out of Pages

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
-import Navbar from './components/Navbar/Navbar';
-import ColorMix from './components/ColorMix/ColorMix';
-import ColorScroll from './components/ColorScroll/ColorScroll';
+import Navbar from '../components/Navbar/Navbar';
+import ColorMix from '../components/ColorMix/ColorMix';
+import ColorScroll from '../components/ColorScroll/ColorScroll';
 
 export default function Home() {
   return (
