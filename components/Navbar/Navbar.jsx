68842901d25d7279189c008b4ff578281782412e
cameron-morrongiello <cammorrongiello@gmail.com>
2022-07-25 20:00:11 -0600

    Minor fixes

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import styles from './Navbar.module.css';
 const Navbar = () => {
   return (
     <div className={styles.navbarWrapper}>
-      <Link href="/"><div style={{fontWeight: "bold", fontSize: "var(--font-size-30)"}}>colors.wtf</div></Link>
+      <Link href="/"><div className={{fontWeight: "bold", fontSize: "var(--font-size-30)"}}>colors.wtf</div></Link>
       <div className={styles.buttonWrapper}>
         <button className={cn('button')}>explore</button>
         <Link href="/about"><button href="about" className={cn('button')}>about</button></Link>
