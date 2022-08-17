b0d3e3cb7297882d42034bd6b78ef3540bf2f45b
cameron-morrongiello <cammorrongiello@gmail.com>
2022-08-17 10:41:07 -0600

    Improved color query

--- queries/colorQuery.js
+++ queries/colorQuery.js
@@ -1,16 +1,40 @@
+import { clientRgbToContractRgb, hexToRgb, rgbToContractRgb } from '../util/colorConversion';
+
 export default function colorSearchQuery(limit=100, direction, where) {
 
+  const { searchOption, searchValue, filterSelected } = where;
   
   const limitStr = `first: ${limit},`
   const sortStr = `orderBy: createdAt, orderDirection: ${direction},`
   let whereStr;
-  console.log(where.filterSelected)
-  if (where.searchValue != "" && where.filterSelected.length > 0)  {
-     whereStr = `where: {html_in: [${where.filterSelected.map((sel, i) => {return `"${sel}"`})}], ${where.searchOption}_contains_nocase: "${where.searchValue}"}`;
-  } else if (where.searchValue != "" && where.filterSelected.length == 0)  {
-     whereStr = `where: {${where.searchOption}_contains_nocase: "${where.searchValue}"}`;
-  } else if (where.searchValue == "" && where.filterSelected.length > 0)  {
-    whereStr = `where: {html_in: [${where.filterSelected.map((sel) => {return `"${sel}"`})}]}`;
+  let querySeachValue;
+  let querySearchOption;
+
+  if(searchValue != "") {
+    if(searchOption == "rgb") {
+      querySearchOption = "rgb";
+      querySeachValue = clientRgbToContractRgb(searchValue);
+    } else if(searchOption == "owner") {
+      querySearchOption = "owner";
+      querySeachValue = searchValue;
+    } else if(searchOption == "hex") {
+      querySearchOption = "rgb";
+      const {r, g, b} = hexToRgb(searchValue);
+      querySeachValue = rgbToContractRgb(r, g, b);
+      console.log(rgbToContractRgb(r, g, b));
+    } else if(searchOption == "name") {
+      querySearchOption = "name_contains_nocase";
+      querySeachValue = searchValue;
+    }
+  }
+
+
+  if (searchValue != "" && filterSelected.length > 0)  {
+     whereStr = `where: {html_in: [${filterSelected.map((sel) => {return `"${sel}"`})}], ${querySearchOption}: "${querySeachValue}"}`;
+  } else if (searchValue != "" && filterSelected.length == 0)  {
+     whereStr = `where: {${querySearchOption}: "${querySeachValue}"}`;
+  } else if (searchValue == "" && filterSelected.length > 0)  {
+    whereStr = `where: {html_in: [${filterSelected.map((sel) => {return `"${sel}"`})}]}`;
   } else {
     whereStr = "";
   }
