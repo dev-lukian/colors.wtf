0e30ad5e439f7b18d67b78c37ec694c0487cc6c6
cameron-morrongiello <cammorrongiello@gmail.com>
2022-09-14 13:31:18 -0400

    Add parent modal selecttion

--- components/ColorProfile/ColorProfile.jsx
+++ components/ColorProfile/ColorProfile.jsx
@@ -5,10 +5,13 @@ import styles from './ColorProfile.module.css';
 
 import { getCssRgb, getTextRgb, rgbToHex, contractRgbToClientRgb } from '../../util/colorConversion';
 
+import { ModalContext } from '../../context/ModalContext';
+
 import querySubgraph from '../../services/colorsService';
 import { parentsQuery } from '../../queries/colorQuery';
 
 const ColorProfile = ({ color }) => {
+  const [modalColor, setModalColor] = useContext(ModalContext);
   const [animationStep, setAnimationStep] = useState(0);
   const [parent1, setParent1] = useState();
   const [parent2, setParent2] = useState();
@@ -26,26 +29,45 @@ const ColorProfile = ({ color }) => {
       querySubgraph(parentsQuery(color.parent1Id, color.parent2Id)).then((data) => {
         console.log(data)
         if(data.data.parent1.length == 1) {
-          setParent1(data.data.parent1[0]);
+          const parent1Raw = data.data.parent1[0];
+          setParent1({
+            name: parent1Raw.name,
+            rgb: contractRgbToClientRgb(parent1Raw.rgb),
+            owner: parent1Raw.owner.id,
+            html: parent1Raw.html,
+            createdAt: parent1Raw.createdAt,
+            parent1Id: parent1Raw.parent1Id,
+            parent2Id: parent1Raw.parent2Id
+          });
         } else {
           setParent1(null);
         }
         if(data.data.parent2.length == 1) {
-          setParent2(data.data.parent2[0]);
+          const parent2Raw = data.data.parent2[0];
+          setParent2({
+            name: parent2Raw.name,
+            rgb: contractRgbToClientRgb(parent2Raw.rgb),
+            owner: parent2Raw.owner.id,
+            html: parent2Raw.html,
+            createdAt: parent2Raw.createdAt,
+            parent1Id: parent2Raw.parent1Id,
+            parent2Id: parent2Raw.parent2Id
+          });
         } else {
           setParent2(null);
         }
       });
     }
     return () => (mounted = false);
-  },[]);
+  },[color]);
 
   return (
     <div className={styles.profileWrapper}>
       <div className={styles.circlesWrapper}>
         <div
           className={cn(styles.circle, styles.smallCircle, animationStep >= 1 && styles.leftCircleMove)}
-          style={{ backgroundColor: parent1 && getCssRgb(contractRgbToClientRgb(parent1.rgb)) }}
+          style={{ backgroundColor: parent1 && getCssRgb(parent1.rgb), pointerEvents: !parent1 && "none"}}
+          onClick={() => setModalColor(parent1)}
         />
         <div
           className={cn(styles.circle, styles.preview)}
@@ -57,7 +79,8 @@ const ColorProfile = ({ color }) => {
         </div>
         <div
           className={cn(styles.circle, styles.smallCircle, animationStep >= 1 && styles.rightCircleMove)}
-          style={{ backgroundColor: parent2 && getCssRgb(contractRgbToClientRgb(parent2.rgb)) }}
+          style={{ backgroundColor: parent2 && getCssRgb(parent2.rgb), pointerEvents: !parent2 && "none"}}
+          onClick={() => setModalColor(parent2)}
         />
       </div>
       <div className={styles.grid}>
